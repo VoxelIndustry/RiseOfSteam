093186bdab9e5af6a77b1c4c10a9aaf6cfb0567b
mantal <mantal0@outlook.fr>
2016-12-21 20:04:40 +0100

    Src

---
+++ src/main/java/net/qbar/QBar.java
@@ -0,0 +1,19 @@
+package net.qbar;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = QBar.MODID, version = QBar.VERSION)
+public class QBar
+{
+	public static final String MODID = "qbar";
+	public static final String VERSION = "0.1";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+	}
+}
