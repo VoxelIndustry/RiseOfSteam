da8cccec58a268984646b49835be5fd41d6c6209
Ourten <ourtencoop@gmail.com>
2017-01-25 01:37:13 +0100

    Add assembler SteamTank and texture

--- src/main/java/net/qbar/common/tile/TileAssembler.java
+++ src/main/java/net/qbar/common/tile/TileAssembler.java
@@ -1,9 +1,68 @@
 package net.qbar.common.tile;
 
-public class TileAssembler extends TileInventoryBase
+import java.util.List;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.qbar.common.steam.CapabilitySteamHandler;
+import net.qbar.common.steam.SteamTank;
+import net.qbar.common.steam.SteamUtil;
+
+public class TileAssembler extends TileInventoryBase implements ITileInfoProvider
 {
+    private final SteamTank steamTank;
+
     public TileAssembler()
     {
         super("InventoryAssembler", 10);
+
+        this.steamTank = new SteamTank(0, 4000, SteamUtil.AMBIANT_PRESSURE * 2);
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(final NBTTagCompound tag)
+    {
+        super.writeToNBT(tag);
+
+        final NBTTagCompound subTag = new NBTTagCompound();
+        this.steamTank.writeToNBT(subTag);
+        tag.setTag("steamTank", subTag);
+
+        return tag;
+    }
+
+    @Override
+    public void readFromNBT(final NBTTagCompound tag)
+    {
+        super.readFromNBT(tag);
+
+        if (tag.hasKey("steamTank"))
+            this.steamTank.readFromNBT(tag.getCompoundTag("steamTank"));
+    }
+
+    @Override
+    public boolean hasCapability(final Capability<?> capability, final EnumFacing facing)
+    {
+        if (capability == CapabilitySteamHandler.STEAM_HANDLER_CAPABILITY && facing == EnumFacing.UP)
+            return true;
+        return super.hasCapability(capability, facing);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T getCapability(final Capability<T> capability, final EnumFacing facing)
+    {
+        if (capability == CapabilitySteamHandler.STEAM_HANDLER_CAPABILITY)
+            return (T) this.steamTank;
+        return super.getCapability(capability, facing);
+    }
+
+    @Override
+    public void addInfo(final List<String> lines)
+    {
+        lines.add("Steam " + this.steamTank.getSteam() + " / " + this.steamTank.getCapacity());
+        lines.add("Pressure " + SteamUtil.pressureFormat.format(this.steamTank.getPressure()) + " / "
+                + SteamUtil.pressureFormat.format(this.steamTank.getMaxPressure()));
     }
 }
