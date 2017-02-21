47312d6c32d7074ca5c089817269df1f5b49637b
Ourten <ourtencoop@gmail.com>
2017-02-21 23:14:44 +0100

    Add item splitter model

--- src/main/java/net/qbar/common/block/BlockSplitter.java
+++ src/main/java/net/qbar/common/block/BlockSplitter.java
@@ -1,15 +1,39 @@
 package net.qbar.common.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.qbar.common.tile.TileSplitter;
 
 public class BlockSplitter extends BlockOrientableMachine
 {
+    protected static final AxisAlignedBB AABB_BOTTOM_HALF = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 14 / 16D, 1.0D);
+
     public BlockSplitter()
     {
         super("itemsplitter", Material.IRON, true, false);
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(final IBlockState state, final IBlockAccess source, final BlockPos pos)
+    {
+        return BlockSplitter.AABB_BOTTOM_HALF;
+    }
+
+    @Override
+    public boolean isOpaqueCube(final IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(final IBlockState state)
+    {
+        return false;
     }
 
     @Override
