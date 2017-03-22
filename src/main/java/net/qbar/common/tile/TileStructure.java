23e38f828d681f300f62d750448d26e4c4b44196
Ourten <ourtencoop@gmail.com>
2017-03-22 10:39:36 +0100

    Fix structure renderbox

--- src/main/java/net/qbar/common/tile/TileStructure.java
+++ src/main/java/net/qbar/common/tile/TileStructure.java
@@ -12,6 +12,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumFacing.Axis;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
@@ -79,9 +80,7 @@ public class TileStructure extends QBarTileBase implements ITileMultiblockCore
             {
                 if (this.getBlueprintState()
                         .getCurrentStep() < this.getBlueprintState().getBlueprint().getSteps().size() - 1)
-                {
                     this.getBlueprintState().setCurrentStep(this.getBlueprintState().getCurrentStep() + 1);
-                }
                 else
                 {
                     final BlockMultiblockBase block = (BlockMultiblockBase) Block
@@ -196,11 +195,19 @@ public class TileStructure extends QBarTileBase implements ITileMultiblockCore
     {
         if (this.blueprint != null && this.cachedBB == null)
         {
-            this.cachedBB = new AxisAlignedBB(
-                    this.pos.add(-this.blueprint.getMultiblock().getOffsetX(),
-                            -this.blueprint.getMultiblock().getOffsetY(), -this.blueprint.getMultiblock().getOffsetZ()),
-                    this.pos.add(this.blueprint.getMultiblock().getWidth(), this.blueprint.getMultiblock().getHeight(),
-                            this.blueprint.getMultiblock().getLength()));
+            if (BlockMultiblockBase.getFacing(this.meta).getAxis() == Axis.Z)
+            {
+                this.cachedBB = new AxisAlignedBB(this.pos.add(-this.blueprint.getMultiblock().getOffsetX(),
+                        -this.blueprint.getMultiblock().getOffsetY(), -this.blueprint.getMultiblock().getOffsetZ()),
+                        this.pos.add(this.blueprint.getMultiblock().getWidth(),
+                                this.blueprint.getMultiblock().getHeight(),
+                                this.blueprint.getMultiblock().getLength()));
+            }
+            else
+                this.cachedBB = new AxisAlignedBB(this.pos.add(-this.blueprint.getMultiblock().getOffsetZ(),
+                        -this.blueprint.getMultiblock().getOffsetY(), -this.blueprint.getMultiblock().getOffsetX()),
+                        this.pos.add(this.blueprint.getMultiblock().getLength(),
+                                this.blueprint.getMultiblock().getHeight(), this.blueprint.getMultiblock().getWidth()));
         }
         if (this.cachedBB != null)
             return this.cachedBB;
@@ -221,18 +228,12 @@ public class TileStructure extends QBarTileBase implements ITileMultiblockCore
             final IBakedModel model = RenderStructure.blockRender.getModelForState(state);
 
             if (this.getBlueprintState().getMultiblockStep() != null)
-            {
                 this.quadsCache = model.getQuads(((BlockMultiblockBase) state.getBlock()).getGhostState(state,
                         this.getBlueprintState().getMultiblockStep().getAlphaState()), null, 0);
-
-                System.out.println("FACE: " + this.getBlueprintState().getMultiblockStep().getOpaqueState());
-            }
             else
                 this.quadsCache = new ArrayList<>(0);
 
             this.previousStep = this.getBlueprintState().getCurrentStep();
-
-            System.out.println("Rebaked!");
         }
         return this.quadsCache;
     }
