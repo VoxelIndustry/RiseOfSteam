2fdd42b9d0fcc56c776e2b2e87aefbf21edf9109
Ourten <ourtencoop@gmail.com>
2017-04-08 01:47:45 +0200

    First try of the mirror rotation

--- src/main/java/net/qbar/common/block/BlockSolarMirror.java
+++ src/main/java/net/qbar/common/block/BlockSolarMirror.java
@@ -1,16 +1,47 @@
 package net.qbar.common.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.model.TRSRTransformation;
+import net.minecraftforge.common.property.ExtendedBlockState;
+import net.minecraftforge.common.property.IExtendedBlockState;
+import net.minecraftforge.common.property.IUnlistedProperty;
+import net.minecraftforge.common.property.Properties;
+import net.qbar.common.IWrenchable;
 import net.qbar.common.tile.machine.TileSolarMirror;
 
-public class BlockSolarMirror extends BlockMachineBase
+public class BlockSolarMirror extends BlockMachineBase implements IWrenchable
 {
     public BlockSolarMirror()
     {
         super("solar_mirror", Material.GLASS);
+    }
+
+    @Override
+    public IBlockState getExtendedState(IBlockState state, IBlockAccess world, BlockPos pos)
+    {
+        if (world.getTileEntity(pos) != null && world.getTileEntity(pos) instanceof TileSolarMirror)
+        {
+            TileSolarMirror solarMirror = (TileSolarMirror) world.getTileEntity(pos);
+            if (solarMirror.transform != TRSRTransformation.identity())
+                return ((IExtendedBlockState) state).withProperty(Properties.AnimationProperty, solarMirror.transform);
+        }
+        return state;
+    }
+
+    @Override
+    public BlockStateContainer createBlockState()
+    {
+        return new ExtendedBlockState(this, new IProperty[0], new IUnlistedProperty[] { Properties.AnimationProperty });
     }
 
     @Override
@@ -35,5 +66,13 @@ public class BlockSolarMirror extends BlockMachineBase
     public TileEntity createNewTileEntity(final World worldIn, final int meta)
     {
         return new TileSolarMirror();
+    }
+
+    @Override
+    public boolean onWrench(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing,
+            IBlockState state)
+    {
+        ((TileSolarMirror)world.getTileEntity(pos)).setSolarBoilerPos(player.getPosition());
+        return false;
     }
 }
--- src/main/java/net/qbar/common/tile/machine/TileSolarMirror.java
+++ src/main/java/net/qbar/common/tile/machine/TileSolarMirror.java
@@ -1,8 +1,80 @@
 package net.qbar.common.tile.machine;
 
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.model.TRSRTransformation;
 import net.qbar.common.tile.QBarTileBase;
+
+import javax.vecmath.AxisAngle4d;
+import javax.vecmath.Matrix4f;
+import javax.vecmath.Quat4f;
 
 public class TileSolarMirror extends QBarTileBase
 {
+    public TRSRTransformation transform = TRSRTransformation.identity();
+    private BlockPos          solarBoilerPos;
 
+    public TileSolarMirror()
+    {
+        this.solarBoilerPos = BlockPos.ORIGIN;
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(final NBTTagCompound tag)
+    {
+        super.writeToNBT(tag);
+
+        tag.setLong("solarBoilerPos", this.solarBoilerPos.toLong());
+        return tag;
+    }
+
+    @Override
+    public void readFromNBT(final NBTTagCompound tag)
+    {
+        super.readFromNBT(tag);
+
+        BlockPos previous = this.solarBoilerPos;
+        this.solarBoilerPos = BlockPos.fromLong(tag.getLong("solarBoilerPos"));
+        if (this.isClient() && !previous.equals(this.solarBoilerPos))
+            this.updateState();
+    }
+
+    public BlockPos getSolarBoilerPos()
+    {
+        return solarBoilerPos;
+    }
+
+    public void setSolarBoilerPos(BlockPos solarBoilerPos)
+    {
+        this.solarBoilerPos = solarBoilerPos;
+        this.sync();
+    }
+
+    public void updateState()
+    {
+        if (this.isServer())
+        {
+            this.sync();
+            return;
+        }
+
+        BlockPos boilerPos = new BlockPos(this.getSolarBoilerPos().getX(), this.getPos().getY(), this.getSolarBoilerPos().getZ());
+        BlockPos lookVec = boilerPos.subtract(this.getPos());
+        
+        AxisAngle4d yaw = new AxisAngle4d(0, 1, 0, -(Math.atan2(lookVec.getZ(), lookVec.getX()) - Math.PI));
+        AxisAngle4d pitch = new AxisAngle4d(1, 0, 0, -(Math.atan2(lookVec.getY(),
+                Math.sqrt(lookVec.getX() * lookVec.getX() + lookVec.getZ() * lookVec.getZ()))));
+        Quat4f rot = new Quat4f(0, 0, 0, 1);
+        Quat4f yawQuat = new Quat4f();
+        Quat4f pitchQuat = new Quat4f();
+        yawQuat.set(yaw);
+        rot.mul(yawQuat);
+        pitchQuat.set(pitch);
+        rot.mul(pitchQuat);
+        Matrix4f matrix = new Matrix4f();
+        matrix.setIdentity();
+        matrix.setRotation(rot);
+        transform = TRSRTransformation.blockCenterToCorner(new TRSRTransformation(matrix));
+        this.world.markBlockRangeForRenderUpdate(this.pos, this.pos);
+    }
 }
