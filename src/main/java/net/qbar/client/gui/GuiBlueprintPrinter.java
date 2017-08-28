d5c648c81ceef30a841b9d0ae6982e649903ecb5
Ourten <ourtencoop@gmail.com>
2017-08-28 23:19:11 +0200

    Add machine type filter to blueprintprinter ui

--- src/main/java/net/qbar/client/gui/GuiBlueprintPrinter.java
+++ src/main/java/net/qbar/client/gui/GuiBlueprintPrinter.java
@@ -12,7 +12,6 @@ import net.qbar.common.machine.QBarMachines;
 import net.qbar.common.multiblock.blueprint.Blueprint;
 import net.qbar.common.tile.machine.TileBlueprintPrinter;
 import org.yggard.brokkgui.control.GuiToggleGroup;
-import org.yggard.brokkgui.element.GuiCheckbox;
 import org.yggard.brokkgui.element.GuiRadioButton;
 import org.yggard.brokkgui.paint.Background;
 import org.yggard.brokkgui.paint.Texture;
@@ -29,15 +28,16 @@ import java.util.stream.Collectors;
 
 public class GuiBlueprintPrinter extends BrokkGuiContainer<BuiltContainer>
 {
-    private static final int xSize = 176, ySize = 166;
+    private static final int           xSize      = 176, ySize = 166;
 
-    private static final Texture BACKGROUND = new Texture(QBar.MODID + ":textures/gui/blueprintprinter.png", 0, 0,
+    private static final Texture       BACKGROUND = new Texture(QBar.MODID + ":textures/gui/blueprintprinter.png", 0, 0,
             xSize / 256.0f, ySize / 256.0f);
 
     private final TileBlueprintPrinter blueprintPrinter;
 
-    private final GuiAbsolutePane            blueprintPane;
-    private       BaseProperty<EMachineType> selectedType;
+    private final GuiAbsolutePane      blueprintPane;
+    private final GuiAbsolutePane      buttonPane;
+    private BaseProperty<EMachineType> selectedType;
 
     public GuiBlueprintPrinter(final EntityPlayer player, final TileBlueprintPrinter blueprintPrinter)
     {
@@ -60,11 +60,18 @@ public class GuiBlueprintPrinter extends BrokkGuiContainer<BuiltContainer>
 
         this.blueprintPane = new GuiAbsolutePane();
         this.blueprintPane.setWidth(176);
-        this.blueprintPane.setHeight(83);
-        body.addChild(this.blueprintPane, 0, 0);
+        this.blueprintPane.setHeight(65);
+        body.addChild(this.blueprintPane, 0, 18);
+
+        this.buttonPane = new GuiAbsolutePane();
+        this.buttonPane.setWidth(176);
+        this.buttonPane.setHeight(18);
+        body.addChild(this.buttonPane, 0, 0);
 
         this.initButtons();
         this.initBlueprints(blueprintPane);
+
+        this.selectedType.addListener((obs, oldValue, newValue) -> this.initBlueprints(blueprintPane));
     }
 
     private void initButtons()
@@ -79,13 +86,12 @@ public class GuiBlueprintPrinter extends BrokkGuiContainer<BuiltContainer>
         {
             GuiRadioButton button = new GuiRadioButton();
             button.setToggleGroup(toggleGroup);
-            button.setTextColor();
 
-            this.blueprintPane.addChild(button, 7 + 18 * i, 0);
+            this.buttonPane.addChild(button, 7 + 18 * i, 0);
             i++;
         }
-        toggleGroup.getSelectedButtonProperty().addListener(obs -> this.selectedType.setValue(
-                EMachineType.values()[toggleGroup.getButtonList().indexOf(toggleGroup.getSelectedButton())]));
+        toggleGroup.getSelectedButtonProperty().addListener((obs, oldValue, newValue) -> this.selectedType.setValue(
+                newValue != null ? EMachineType.values()[toggleGroup.getButtonList().indexOf(newValue)] : null));
     }
 
     private void initBlueprints(GuiAbsolutePane mainPane)
@@ -95,30 +101,36 @@ public class GuiBlueprintPrinter extends BrokkGuiContainer<BuiltContainer>
         int maxTier = blueprintList.stream().max(Comparator.comparingInt(descriptor -> descriptor.getTier().ordinal()))
                 .get().getTier().ordinal();
 
-        int currentHeight = 25;
+        int currentHeight = 7;
+        mainPane.clearChilds();
         for (int i = 0; i <= maxTier; i++)
         {
             int currentTier = i;
-            long tierElements = blueprintList.stream().filter(descriptor ->
-                    descriptor.getTier().ordinal() == currentTier).count();
+            long tierElements = blueprintList.stream()
+                    .filter(descriptor -> descriptor.getTier().ordinal() == currentTier).count();
 
             GuiAbsolutePane tierPane = new GuiAbsolutePane();
             tierPane.setWidth(176);
             tierPane.setHeight(18 * (int) Math.ceil(tierElements / 9D));
 
-            List<MachineDescriptor> collect = blueprintList.stream().filter(descriptor ->
-                    descriptor.getTier().ordinal() == currentTier)
-                    .sorted(Comparator.comparing(MachineDescriptor::getType)).collect(Collectors.toList());
+            List<MachineDescriptor> collect = null;
+            if (this.selectedType.getValue() == null)
+                collect = blueprintList.stream().filter(descriptor -> descriptor.getTier().ordinal() == currentTier)
+                        .sorted(Comparator.comparing(MachineDescriptor::getType)).collect(Collectors.toList());
+            else
+                collect = blueprintList.stream().filter(descriptor -> descriptor.getTier().ordinal() == currentTier
+                        && descriptor.getType() == this.selectedType.getValue()).collect(Collectors.toList());
             for (int j = 0; j < collect.size(); j++)
             {
                 MachineDescriptor machineDescriptor = collect.get(j);
-                ItemStackView itemStack = new ItemStackView(new ItemStack(Item.getByNameOrId(QBar.MODID + ":" +
-                        machineDescriptor.getName())));
+                ItemStackView itemStack = new ItemStackView(
+                        new ItemStack(Item.getByNameOrId(QBar.MODID + ":" + machineDescriptor.getName())));
                 itemStack.setWidth(18);
                 itemStack.setHeight(18);
                 itemStack.setTooltip(true);
                 itemStack.setAlternateString("");
-                ((GuiBehaviorSkinBase) itemStack.getSkin()).setBackground(new Background(machineDescriptor.getType().getColor()));
+                ((GuiBehaviorSkinBase) itemStack.getSkin())
+                        .setBackground(new Background(machineDescriptor.getType().getColor()));
 
                 tierPane.addChild(itemStack, 7 + 18 * (j % 9), 18 * (j / 9));
             }
