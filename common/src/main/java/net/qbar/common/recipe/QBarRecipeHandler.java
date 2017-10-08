385a5d2cd54f80ea12d80bb80fe52300e0eac3e7
Ourten <ourtencoop@gmail.com>
2017-10-08 23:54:32 +0200

    Document the RecipeHandler

--- common/src/main/java/net/qbar/common/recipe/QBarRecipeHandler.java
+++ common/src/main/java/net/qbar/common/recipe/QBarRecipeHandler.java
@@ -21,6 +21,20 @@ public class QBarRecipeHandler
 
     public static final ArrayList<IRecipe> CRAFTING_RECIPES = new ArrayList<>();
 
+    /**
+     * Check if a given indexed ingredient match a recipe.
+     * Only shaped recipes are currently supported.
+     * The matching logic is deferred to the <code>RecipeCategory</code> which by default implementation
+     * will defer this logic to the <code>RecipeIngredient</code> wrapper after retrieving the specified index.
+     * This method does not take into account the quantity of the ingredient.
+     *
+     * @param recipeID   of the category to check
+     * @param recipeSlot of the ingredient
+     * @param ingredient to match
+     * @param <T>        a generic ingredient that can be wrapped by the recipe in a <code>RecipeIngredient</code>
+     *                   instance
+     * @return the result of the match
+     */
     @SuppressWarnings("unchecked")
     public static <T> boolean inputMatchWithoutCount(final String recipeID, final int recipeSlot, final T ingredient)
     {
@@ -28,6 +42,21 @@ public class QBarRecipeHandler
                 QBarRecipeHandler.RECIPES.get(recipeID).inputMatchWithoutCount(recipeSlot, ingredient);
     }
 
+    /**
+     * Check if a given indexed ingredient match a recipe.
+     * Only shaped recipes are currently supported.
+     * The matching logic is deferred to the <code>RecipeCategory</code> which by default implementation
+     * will defer this logic to the <code>RecipeIngredient</code> wrapper after retrieving the specified index.
+     * This method does take into account the quantity of the ingredient,
+     * it must be at least equals or superior to the recipe expected quantity.
+     *
+     * @param recipeID   of the category to check
+     * @param recipeSlot of the ingredient
+     * @param ingredient to match
+     * @param <T>        a generic ingredient that can be wrapped by the recipe in a <code>RecipeIngredient</code>
+     *                   instance
+     * @return the result of the match
+     */
     @SuppressWarnings("unchecked")
     public static <T> boolean inputMatchWithCount(final String recipeID, final int recipeSlot, final T ingredient)
     {
@@ -35,6 +64,18 @@ public class QBarRecipeHandler
                 QBarRecipeHandler.RECIPES.get(recipeID).inputMatchWithCount(recipeSlot, ingredient);
     }
 
+    /**
+     * Retrieve a specific <code>QBarRecipe</code> from the category containing it and a list of generic ingredients.
+     * By default implementation the <code>RecipeCategory</code> will attempt to match the list of ingredients
+     * against all its registered recipes.
+     * Many custom categories will return a fake recipe for dynamic evaluation.
+     * <p>
+     * This method has a high performance cost and its return should be always cached.
+     *
+     * @param recipeID of the category to check
+     * @param inputs   an index-ordered list of arbitrary generic ingredients
+     * @return an Optional containing the found recipe or null in case of failure
+     */
     @SuppressWarnings("unchecked")
     public static Optional<QBarRecipe> getRecipe(final String recipeID, final Object... inputs)
     {
