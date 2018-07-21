21c5b8e40af61b77c6f47096a0d960f14e89be4c
Ourten <ourtencoop@gmail.com>
2018-07-21 14:11:31 +0200

    Make veins gen less scattered

--- content/world/src/main/java/net/ros/common/world/OreVeins.java
+++ content/world/src/main/java/net/ros/common/world/OreVeins.java
@@ -2,13 +2,17 @@ package net.ros.common.world;
 
 import com.google.common.collect.Lists;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Biomes;
 import net.ros.common.block.BlockVeinOre;
 import net.ros.common.init.ROSBlocks;
-import net.ros.common.ore.Ores;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
+
+import static net.minecraft.init.Biomes.*;
+import static net.ros.common.ore.Ores.*;
+import static net.ros.common.world.EVeinForm.UPWARD;
+import static net.ros.common.world.EVeinHeapForm.PLATES;
+import static net.ros.common.world.EVeinHeapForm.SPHERES;
 
 public class OreVeins
 {
@@ -32,59 +36,58 @@ public class OreVeins
     {
         initMarkers();
 
-        IRON_NICKEL = new OreVeinDescriptor("iron-nickel").veinForm(EVeinForm.FLAT).heapForm(EVeinHeapForm.PLATES)
-                .heapQty(5).heapDensity(0.8f).heapSize(10).rarity(0.002f).heightRange(4, 64).richChance(0.1f, 0.8f)
+        IRON_NICKEL = new OreVeinDescriptor("iron-nickel").veinForm(UPWARD).heapForm(PLATES)
+                .heapQty(4).heapDensity(0.8f).heapSize(5).rarity(0.002f).heightRange(4, 64).richChance(0.1f, 0.8f)
                 .poorChance(0, 0.8f)
-                .content(((BlockVeinOre) ROSBlocks.IRON_NICKEL_ORE).getStateFromOre(Ores.PENTLANDITE), 0.3f)
-                .content(((BlockVeinOre) ROSBlocks.IRON_NICKEL_ORE).getStateFromOre(Ores.GARNIERITE), 0.3f)
-                .content(((BlockVeinOre) ROSBlocks.IRON_NICKEL_ORE).getStateFromOre(Ores.LATERITE), 0.4f)
-                .biomes(BiomeMatcher.fromBiomes(Biomes.JUNGLE, Biomes.JUNGLE_HILLS, Biomes.JUNGLE_EDGE, Biomes.SAVANNA,
-                        Biomes.SAVANNA_PLATEAU))
+                .content(((BlockVeinOre) ROSBlocks.IRON_NICKEL_ORE).getStateFromOre(PENTLANDITE), 0.3f)
+                .content(((BlockVeinOre) ROSBlocks.IRON_NICKEL_ORE).getStateFromOre(GARNIERITE), 0.3f)
+                .content(((BlockVeinOre) ROSBlocks.IRON_NICKEL_ORE).getStateFromOre(LATERITE), 0.4f)
+                .biomes(BiomeMatcher.fromBiomes(JUNGLE, JUNGLE_HILLS, JUNGLE_EDGE, SAVANNA, SAVANNA_PLATEAU))
                 .marker(IRON_DIRT_MARKER).createBlockSupplier();
 
-        IRON_COPPER = new OreVeinDescriptor("iron-copper").veinForm(EVeinForm.FLAT).heapForm(EVeinHeapForm.PLATES)
-                .heapQty(5).heapDensity(0.7f).heapSize(10).rarity(0.002f).heightRange(4, 64).richChance(0.1f, 0.8f)
+        IRON_COPPER = new OreVeinDescriptor("iron-copper").veinForm(UPWARD).heapForm(PLATES)
+                .heapQty(4).heapDensity(0.7f).heapSize(5).rarity(0.002f).heightRange(4, 64).richChance(0.1f, 0.8f)
                 .poorChance(0, 0.8f)
-                .content(((BlockVeinOre) ROSBlocks.IRON_COPPER_ORE).getStateFromOre(Ores.CHALCOPYRITE), 0.3f)
-                .content(((BlockVeinOre) ROSBlocks.IRON_COPPER_ORE).getStateFromOre(Ores.TETRAHEDRITE), 0.3f)
-                .content(((BlockVeinOre) ROSBlocks.IRON_COPPER_ORE).getStateFromOre(Ores.MALACHITE), 0.4f)
-                .biomes(BiomeMatcher.fromBiomes(Biomes.RIVER, Biomes.FROZEN_RIVER, Biomes.SWAMPLAND,
-                        Biomes.MUTATED_SWAMPLAND, Biomes.FOREST_HILLS, Biomes.BIRCH_FOREST_HILLS, Biomes.JUNGLE_HILLS,
-                        Biomes.EXTREME_HILLS, Biomes.TAIGA_HILLS))
+                .content(((BlockVeinOre) ROSBlocks.IRON_COPPER_ORE).getStateFromOre(CHALCOPYRITE), 0.3f)
+                .content(((BlockVeinOre) ROSBlocks.IRON_COPPER_ORE).getStateFromOre(TETRAHEDRITE), 0.3f)
+                .content(((BlockVeinOre) ROSBlocks.IRON_COPPER_ORE).getStateFromOre(MALACHITE), 0.4f)
+                .biomes(BiomeMatcher.fromBiomes(RIVER, FROZEN_RIVER, SWAMPLAND,
+                        MUTATED_SWAMPLAND, FOREST_HILLS, BIRCH_FOREST_HILLS, JUNGLE_HILLS,
+                        EXTREME_HILLS, TAIGA_HILLS))
                 .marker(COPPER_SAND_MARKER).createBlockSupplier();
 
-        TIN = new OreVeinDescriptor("tin").veinForm(EVeinForm.UPWARD).heapForm(EVeinHeapForm.PLATES).heapQty(5)
-                .heapDensity(0.5f).heapSize(15).rarity(0.001f).heightRange(4, 64).richChance(0.1f, 0.8f).poorChance
+        TIN = new OreVeinDescriptor("tin").veinForm(UPWARD).heapForm(PLATES).heapQty(5)
+                .heapDensity(0.5f).heapSize(7).rarity(0.001f).heightRange(4, 64).richChance(0.1f, 0.8f).poorChance
                         (0, 0.8f)
-                .content(((BlockVeinOre) ROSBlocks.TIN_ORE).getStateFromOre(Ores.CASSITERITE), 1)
-                .content(((BlockVeinOre) ROSBlocks.TIN_ORE).getStateFromOre(Ores.TEALLITE), 1)
-                .biomes(BiomeMatcher.fromBiomes(Biomes.FOREST, Biomes.FOREST_HILLS, Biomes.BIRCH_FOREST_HILLS,
-                        Biomes.BIRCH_FOREST, Biomes.EXTREME_HILLS, Biomes.EXTREME_HILLS_EDGE,
-                        Biomes.EXTREME_HILLS_WITH_TREES))
+                .content(((BlockVeinOre) ROSBlocks.TIN_ORE).getStateFromOre(CASSITERITE), 1)
+                .content(((BlockVeinOre) ROSBlocks.TIN_ORE).getStateFromOre(TEALLITE), 1)
+                .biomes(BiomeMatcher.fromBiomes(FOREST, FOREST_HILLS, BIRCH_FOREST_HILLS,
+                        BIRCH_FOREST, EXTREME_HILLS, EXTREME_HILLS_EDGE,
+                        EXTREME_HILLS_WITH_TREES))
                 .marker(TIN_CLAY_MARKER).marker(TIN_SAND_MARKER).createBlockSupplier();
 
-        IRON_ZINC = new OreVeinDescriptor("iron-zinc").veinForm(EVeinForm.FLAT).heapForm(EVeinHeapForm.PLATES)
-                .heapQty(6).heapDensity(0.75f).heapSize(13).rarity(0.0005f).heightRange(4, 64).richChance(0.1f, 0.8f)
+        IRON_ZINC = new OreVeinDescriptor("iron-zinc").veinForm(UPWARD).heapForm(PLATES)
+                .heapQty(4).heapDensity(0.75f).heapSize(6).rarity(0.0005f).heightRange(4, 64).richChance(0.1f, 0.8f)
                 .poorChance(0, 0.8f)
-                .content(((BlockVeinOre) ROSBlocks.IRON_ZINC_ORE).getStateFromOre(Ores.SPHALERITE), 1)
-                .biomes(BiomeMatcher.fromBiomes(Biomes.FOREST, Biomes.FOREST_HILLS, Biomes.BIRCH_FOREST,
-                        Biomes.BIRCH_FOREST_HILLS, Biomes.PLAINS, Biomes.OCEAN, Biomes.DEEP_OCEAN, Biomes.FROZEN_OCEAN,
-                        Biomes.EXTREME_HILLS, Biomes.EXTREME_HILLS_WITH_TREES, Biomes.EXTREME_HILLS_EDGE))
+                .content(((BlockVeinOre) ROSBlocks.IRON_ZINC_ORE).getStateFromOre(SPHALERITE), 1)
+                .biomes(BiomeMatcher.fromBiomes(FOREST, FOREST_HILLS, BIRCH_FOREST,
+                        BIRCH_FOREST_HILLS, PLAINS, OCEAN, DEEP_OCEAN, FROZEN_OCEAN,
+                        EXTREME_HILLS, EXTREME_HILLS_WITH_TREES, EXTREME_HILLS_EDGE))
                 .createBlockSupplier();
 
-        GOLD = new OreVeinDescriptor("gold").veinForm(EVeinForm.SCATTERED).heapForm(EVeinHeapForm.SCATTERED).heapQty(1)
-                .heapDensity(0.1f).heapSize(20).rarity(0.0001f).heightRange(4, 64).richChance(0, 0.6f).poorChance
+        GOLD = new OreVeinDescriptor("gold").veinForm(UPWARD).heapForm(EVeinHeapForm.SCATTERED).heapQty(2)
+                .heapDensity(0.1f).heapSize(15).rarity(0.0001f).heightRange(4, 64).richChance(0, 0.6f).poorChance
                         (0.2f, 1f)
-                .content(((BlockVeinOre) ROSBlocks.GOLD_ORE).getStateFromOre(Ores.GOLD_ORE), 1)
+                .content(((BlockVeinOre) ROSBlocks.GOLD_ORE).getStateFromOre(GOLD_ORE), 1)
                 .biomes(BiomeMatcher.WILDCARD)
                 .marker(GOLD_ROCK_MARKER).createBlockSupplier();
 
-        REDSTONE = new OreVeinDescriptor("redstone").veinForm(EVeinForm.SCATTERED).heapForm(EVeinHeapForm.SPHERES)
-                .heapQty(3).heapDensity(0.3f).heapSize(20).rarity(0.0001f).heightRange(4, 64).richChance(0.1f, 0.8f)
+        REDSTONE = new OreVeinDescriptor("redstone").veinForm(UPWARD).heapForm(SPHERES)
+                .heapQty(3).heapDensity(0.3f).heapSize(12).rarity(0.0001f).heightRange(4, 64).richChance(0.1f, 0.8f)
                 .poorChance(0, 0.8f)
-                .content(((BlockVeinOre) ROSBlocks.REDSTONE_ORE).getStateFromOre(Ores.REDSTONE_ORE), 1)
-                .biomes(BiomeMatcher.fromBiomes(Biomes.PLAINS, Biomes.SWAMPLAND, Biomes.FOREST, Biomes.FOREST_HILLS,
-                        Biomes.BIRCH_FOREST, Biomes.BIRCH_FOREST_HILLS))
+                .content(((BlockVeinOre) ROSBlocks.REDSTONE_ORE).getStateFromOre(REDSTONE_ORE), 1)
+                .biomes(BiomeMatcher.fromBiomes(PLAINS, SWAMPLAND, FOREST, FOREST_HILLS,
+                        BIRCH_FOREST, BIRCH_FOREST_HILLS))
                 .marker(REDSTONE_GRASS_MARKER).createBlockSupplier();
 
         VEINS = Lists.newArrayList(IRON_NICKEL, IRON_COPPER, TIN, IRON_ZINC, GOLD, REDSTONE);
@@ -101,7 +104,7 @@ public class OreVeins
 
         VeinBlockSupplier copperBlockSupplier = (rand, veinSize, centerOffset) -> FeatureGenerator.randomState(rand,
                 Lists
-                .newArrayList(Pair.of(ROSBlocks.ORE_SAND.getStateFromMeta(0), 1f)));
+                        .newArrayList(Pair.of(ROSBlocks.ORE_SAND.getStateFromMeta(0), 1f)));
         COPPER_SAND_MARKER = (world, pos, actualHeapSize, heapSize) -> FeatureGenerator.generatePlate(world, world
                         .getTopSolidOrLiquidBlock(pos),
                 copperBlockSupplier, (int) (4 * ((float) actualHeapSize / heapSize)), 3, 0.8f,
@@ -109,7 +112,7 @@ public class OreVeins
 
         VeinBlockSupplier tinClayBlockSupplier = (rand, veinSize, centerOffset) -> FeatureGenerator.randomState(rand,
                 Lists
-                .newArrayList(Pair.of(ROSBlocks.ORE_CLAY.getStateFromMeta(0), 1f)));
+                        .newArrayList(Pair.of(ROSBlocks.ORE_CLAY.getStateFromMeta(0), 1f)));
         TIN_CLAY_MARKER = (world, pos, actualHeapSize, heapSize) -> FeatureGenerator.generatePlate(world, world
                         .getTopSolidOrLiquidBlock(pos),
                 tinClayBlockSupplier, (int) (3 * ((float) actualHeapSize / heapSize)), 3, 0.8f,
@@ -117,7 +120,7 @@ public class OreVeins
 
         VeinBlockSupplier tinSandBlockSupplier = (rand, veinSize, centerOffset) -> FeatureGenerator.randomState(rand,
                 Lists
-                .newArrayList(Pair.of(ROSBlocks.ORE_SAND.getStateFromMeta(1), 1f)));
+                        .newArrayList(Pair.of(ROSBlocks.ORE_SAND.getStateFromMeta(1), 1f)));
         TIN_SAND_MARKER = (world, pos, actualHeapSize, heapSize) -> FeatureGenerator.generatePlate(world, world
                         .getTopSolidOrLiquidBlock(pos),
                 tinSandBlockSupplier, (int) (3 * ((float) actualHeapSize / heapSize)), 3, 0.8f,
